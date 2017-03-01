536a08c84513c1574a54839a42b85346fcef6cd1
jvssvarma <jampana.varma49@gmail.com>
2017-03-01 10:52:57 -0800

    Add sentence validations

--- annkissam.rb
+++ annkissam.rb
@@ -22,9 +22,29 @@ class Annkissam
   end
 
   def valid_sentence(str, dictionary)
-    sentences(str, dictionary)
+    final_result = []
+    sentences(str, dictionary).each do |a|
+      set = a.split(" ")
+      if all_words_valid?(set) && verb_correct?(set) && noun_articles_correct?(set)
+        final_result << a
+      end
+    end
+    final_result.sort! #This can be returned without any sort but it added because it looks cool.
   end
 
+  private
+
+  def all_words_valid? set
+    (set & $dictionary) == set
+  end
+
+  def verb_correct? set
+    (set & $verbs).count >= 1
+  end
+
+  def noun_articles_correct? set
+    ((set & $nouns).count >= 1) || ((set & $verbs).count >= 2)
+  end
 end
 
 lets_test = Annkissam.new
